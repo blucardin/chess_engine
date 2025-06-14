fef4204fa18f0c3efe9b7333575fd796cbc1cea7
blucardin <blucardin@gmail.com>
2025-06-14 00:56:41 -0400

    Train the model on the non-resigned dataset

--- chess_neural_network_training/src/bin/train/main.rs
+++ chess_neural_network_training/src/bin/train/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
     let device = burn::backend::cuda::CudaDevice::default(); 
     // println!("CUDA Device: {}", device.index);
-    let artifact_dir = "/model";
+    let artifact_dir = "/Users/sam/RustroverProjects/chess/model";
     chess_neural_network_training::training::train::<MyAutodiffBackend>(
         artifact_dir,
         TrainingConfig::new(ModelConfig::new(), AdamConfig::new()),
